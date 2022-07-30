1af065d357ad7593593bed102f0ae6a38a69f118
Justin Nicholas <nicholasj964@yahoo.com>
2022-07-30 11:30:02 -0500

    got button clicks to add text to input field for all buttons.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,11 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('JQ Wokring');
+    $('.button').on('click', addChar);
+}
+
+function addChar() {
+    let char = $(this).text();
+    console.log(char);
+    $('#calcIn').val($('#calcIn').val() + char);
 }
