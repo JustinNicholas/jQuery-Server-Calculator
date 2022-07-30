5845ff0bd74a1710a6fb12471290e7eca5a6a8e2
Justin Nicholas <nicholasj964@yahoo.com>
2022-07-30 12:22:16 -0500

    equation history is kept on server and get/send working for history. exquations displaying correctly on the DOM.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,7 +5,7 @@ $(document).ready(onReady);
 function onReady() {
     console.log('JQ Wokring');
     $('.button').on('click', addChar);
-    $('#equal').on('click', sendCalc)
+    $('#equal').on('click', sendEquation)
     $('#c').on('click', clearCalc)
 }
 
@@ -19,21 +19,40 @@ function clearCalc() {
     $('#calcIn').val('');
 }
 
-function sendCalc() {
+function sendEquation() {
     console.log('sending calc to server');
-
+    let currentCalc = ($('#calcIn').val());
+    let currentCalcString = currentCalc.toString();
+    console.log(currentCalcString);
     $.ajax({
         method: 'POST',
         url: '/calc',
         data: {
-            calc: $('calcIn').text(),
+            calc: currentCalcString,
         }
     }).then(function(response){
         console.log(response);
-        getAnswer();
+        getEquations();
+    })
+    $('#calcIn').val('');
+}
+
+function getEquations(){
+    console.log('in get answer');
+    $.ajax({
+        method: 'GET',
+        url: '/calc'
+    }).then(function(response){
+        console.log(response);
+        displayEquations(response);
     })
 }
 
-function getAnswer(){
-
+function displayEquations(equations){
+    $('.history').empty();
+    for( let equation of equations ){
+        $('.history').append(`
+            <p>${equation.calc}</p>
+        `)
+    }
 }
--- server/server.js
+++ server/server.js
@@ -6,7 +6,25 @@ app.use(express.static('server/public'));
 
 app.use(express.urlencoded({extended : true}));
 
+let equationHistory = [];
+let answer = '';
+
 app.listen(PORT, () => {
     console.log('listening on PORT:', PORT);
 });
 
+app.post('/calc', (req, res) => {
+    runCalculation();
+    console.log(req.body);
+    equationHistory.push(req.body);
+    res.sendStatus(200);
+})
+
+app.get('/calc', (req, res) => {
+    console.log(equationHistory);
+    res.send(equationHistory);
+})
+
+function runCalculation(){
+ console.log('in runCalc');
+}
