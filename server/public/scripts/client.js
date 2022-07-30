fb6b4a9de754e961d687c893c225e3b0ff1f868f
Justin Nicholas <nicholasj964@yahoo.com>
2022-07-30 11:34:59 -0500

    clear button clearing input field.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,10 +5,35 @@ $(document).ready(onReady);
 function onReady() {
     console.log('JQ Wokring');
     $('.button').on('click', addChar);
+    $('#equal').on('click', sendCalc)
+    $('#c').on('click', clearCalc)
 }
 
 function addChar() {
     let char = $(this).text();
     console.log(char);
     $('#calcIn').val($('#calcIn').val() + char);
+}
+
+function clearCalc() {
+    $('#calcIn').val('');
+}
+
+function sendCalc() {
+    console.log('sending calc to server');
+
+    $.ajax({
+        method: 'POST',
+        url: '/calc',
+        data: {
+            calc: $('calcIn').text(),
+        }
+    }).then(function(response){
+        console.log(response);
+        getAnswer();
+    })
+}
+
+function getAnswer(){
+
 }
